f4c91c08f3dd7cf027f86ec34d6c54608129058b
Tim Barkow <tim@robotloveskitten.com>
2021-04-05 19:49:30 -0500

    65knob first firmware

---
+++ keyboards/rlk/65knob/65knob.h
@@ -0,0 +1,45 @@
+#include "quantum.h"
+
+/* Default layout */
+#define KEYMAP( \
+	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013,       \
+	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113,       \
+	K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213,       \
+	K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, K312,             \
+	K400, K401, K402,             K405,             K408, K409, K410, K411, K412, K413  \
+) { \
+	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011,  K012,  K013 }, \
+	{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111,  K112,  K113 }, \
+	{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  K212,  K213 }, \
+	{ K300,  K301,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  K311,  K312,  KC_NO  }, \
+	{ K400,  K401,  K402,  KC_NO, KC_NO, K405,  KC_NO, KC_NO, K408,  K409,  K410,  K411,  K412,  K413 }  \
+}
+
+
+
+enum terrazzo_matrix_effects {
+    TERRAZZO_NONE = 0,
+// --------------------------------------
+// -----Begin led effect enum macros-----
+#define TERRAZZO_EFFECT(name, ...) TERRAZZO_EFFECT_##name,
+#include "terrazzo_effects/terrazzo_effects.inc"
+#undef TERRAZZO_EFFECT
+    // --------------------------------------
+    // -----End led effect enum macros-------
+    // All new effects go above this line
+    TERRAZZO_EFFECT_MAX
+};
+
+enum terrazzo_keycodes {
+    TZ_NXT = SAFE_RANGE,
+    TZ_PRV,
+    TZ_OFF
+};
+
+void terrazzo_render(void);
+void terrazzo_set_pixel(uint8_t x, uint8_t y, uint8_t value);
+void terrazzo_draw_at(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t image[]);
+void terrazzo_scroll_pixel(bool clockwise);
+void terrazzo_step_mode(void);
+void terrazzo_step_mode_reverse(void);
+void terrazzo_mode_off(void);
---
+++ keyboards/rlk/65knob/config.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "config_common.h"
+
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x524B
+#define PRODUCT_ID      0x0022
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    RobotLovesKitten
+#define PRODUCT         65Knob
+#define DESCRIPTION     65Knob Keyboard
+
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 16
+
+/* key matrix pins */
+#define MATRIX_COL_PINS { B7, B5, B4, E6, D7, D2, D3, B6, F0, F1, C7, D5, B2, B3 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1 }
+
+#define UNUSED_PINS
+#define DIODE_DIRECTION COL2ROW
+
+/* ROTARY ENCODERS */
+
+#define ENCODERS_PAD_A { C6 }
+#define ENCODERS_PAD_B { D4 }
+#define TAP_CODE_DELAY 10
+// #define ENCODER_DIRECTION_FLIP
+
+#define ENCODER_RESOLUTION 2
+#define BACKLIGHT_LEVELS 5
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 0
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+#ifdef LED_MATRIX_ENABLE
+
+#define LED_DRIVER_ADDR_1 0x74
+#define LED_DRIVER_COUNT 1
+#define DRIVER_LED_TOTAL 105
+#define LED_MATRIX_ROWS 15
+#define LED_MATRIX_COLS 7
+#define LED_MATRIX_MAXIMUM_BRIGHTNESS 20
+#define LED_DISABLE_WHEN_USB_SUSPENDED true
+
+#endif
+
+/* Terrazzo animations */
+
+#define DISABLE_TERRAZZO_EFFECT_STRIPES
+#define DISABLE_TERRAZZO_EFFECT_DINO
+#define DISABLE_TERRAZZO_EFFECT_OUTRUN
+// #define DISABLE_TERRAZZO_EFFECT_PAC_DUDE
+// #define DISABLE_TERRAZZO_EFFECT_HEART
+// #define DISABLE_TERRAZZO_EFFECT_WPM_CHART
+// #define DISABLE_TERRAZZO_EFFECT_DOT
