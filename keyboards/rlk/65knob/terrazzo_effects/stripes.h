d603620e5ba21be20a1dc54f3ba55de9cece5db0
Tim Barkow <tim@robotloveskitten.com>
2021-04-12 16:13:02 -0500

    simplified stripes animation. works with encoder now

--- keyboards/rlk/65knob/terrazzo_effects/stripes.h
+++ keyboards/rlk/65knob/terrazzo_effects/stripes.h
@@ -19,14 +19,13 @@ TERRAZZO_EFFECT(STRIPES)
 #   ifdef TERRAZZO_EFFECT_IMPLS
 
 void STRIPES(uint8_t i, bool dir) {
-    uint8_t sweep[] = {1, 2, 3, 4, 6, 8, 10, 15, 20, 30, 40, 60};
-    uint8_t levels = 12;
-    float soften = 5;
+    uint8_t sweep[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    uint8_t levels = 10;
 
     for (int y = 0; y < LED_MATRIX_ROWS; y++) {
         for (int x  = 0; x < LED_MATRIX_COLS; x++) {
             uint8_t target = (x+y+i)%levels;
-            terrazzo_set_pixel(x, y, floor(sweep[target] / soften));
+            terrazzo_set_pixel(x, y, floor(sweep[target]));
         }
     }
 }
